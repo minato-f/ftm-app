9646727a2035c83c614bd6038e9a9a457180d477
EC2 Default User <ec2-user@ip-172-31-24-202.us-east-2.compute.internal>
2020-01-30 06:57:43 +0000

    second commit

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -8,4 +8,24 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+ 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.home');
+    }
+    
 }
